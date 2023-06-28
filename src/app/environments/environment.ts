e582d427345a72ef719e997475ded2d4438ba8ec
WalissonCF <walissoncf44@gmail.com>
2023-06-27 21:36:20 -0300

    feat: criando env

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    openweathermapApiKey: 'b623be2daa37a1e75be2ec86f07012d5',
+    openweathermapApiUrl: 'https://api.openweathermap.org/data/3.0'
+};
