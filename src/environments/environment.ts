57060b051944c4542d81aeca8fae5a9e6726f08e
WalissonCF <walissoncf44@gmail.com>
2023-07-03 22:57:59 -0300

    refactor: remoção do token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    openweathermapApiKey: 'b623be2daa37a1e75be2ec86f07012d5',
+    openweathermapApiKey: '',
     openweathermapApiUrl: 'https://api.openweathermap.org/data/3.0'
 };
